af21cda4226d1b6373234d346fb20f9d69ade3de
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-07-29 17:38:31 -0500

    server now starts

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
-const path = require('path')
-const PORT = process.env.PORT || 3001
+const path = require('path');
+const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3001
+
+app.listen(3001, () => {
+    console.log(`Server now on port 3001!`)
+})
