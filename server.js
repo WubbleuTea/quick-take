ea5dd09c0b81c0599ee0255d2cee7552a34e37bc
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-07-31 15:56:11 -0500

    delete funtionality works

--- server.js
+++ server.js
@@ -24,6 +24,17 @@ app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './db/db.json'))
 });
 
+app.get('/api/notes/:id', (req, res) => {
+    const found = db.some(note => note.id === req.params.id);
+
+    if(found) {
+        res.json(db.filter(note => note.id === req.params.id))
+    } else {
+        res.status(400).json({ msg: `No note found with an id of ${req.params.id}`})
+    }
+})
+
+
 function createNewNote(body, notesArray) {
     const note = body;
     console.log(body)
@@ -41,6 +52,31 @@ app.post('/api/notes', (req, res) => {
     const note = createNewNote(req.body, db)
     res.json(note)
 });
+
+function deleteNote(id, notesArray) {
+
+    notesArray = notesArray.filter((note) => note.id !== id);
+
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray , null, 2)
+    );
+
+    return notesArray;
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+    const toBeDeleted = db.some(note => note.id === req.params.id);
+    console.log(req.params.id)
+    
+
+    if(toBeDeleted) {
+        res.json({ msg: 'Note deleted', db: db.filter(note => note.id !== req.params.id)});
+        deleteNote(req.params.id, db)
+    } else {
+        res.status(400).json({ msg: `Cannot delete this note`})
+    }
+})
 
 app.listen(3001, () => {
     console.log(`Server now on port ${PORT}!`)
